6fd965dca57af283bf1d6883380dd19a81575181
lildavid1 <lild93078@gmail.com>
2023-07-06 09:29:01 -0700

    done 1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line.
- * @s: string
- * Return: no return.
+ * _puts_recursion - Prints a string using recursion
+ * @s: The string to be printed
  */
-
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-		_putchar('\n');
+    if (*s == '\0') /* Base case: end of string */
+    {
+        _putchar('\n'); /* Print newline */
+        return;
+    }
+
+    _putchar(*s); /* Print current character */
+    _puts_recursion(s + 1); /* Recursively call the function with the next character */
 }
+
